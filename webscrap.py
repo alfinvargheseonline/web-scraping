bcec04bcf1b39c1f5f3a47406a488302c2acdccc
alfinvargheseonline <101696840+alfinvargheseonline@users.noreply.github.com>
2023-08-09 22:50:21 +0530

    web scrap

--- webscrap.py
+++ webscrap.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-base_url = "https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&p%5B%5D=facets.brand%255B%255D%3DRealme&otracker=nmenu_sub_Electronics_0_Realme"
+base_url = "https://www.flipkart.com/search?q=mobiles+under+30000&as=on&as-show=on&otracker=AS_Query_OrganicAutoSuggest_5_13_na_na_ps&otracker1=AS_Query_OrganicAutoSuggest_5_13_na_na_ps&as-pos=5&as-type=RECENT&suggestionId=mobiles+under+30000&requestId=f74114cb-9be5-4314-8c96-04ab6f44853b&as-searchtext=mobiles+under+&p%5B%5D=facets.price_range.from%3D20000&p%5B%5D=facets.price_range.to%3DMax"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
@@ -48,6 +48,6 @@ for phone_card in phone_cards:
         print("Error in extracting phone data:", e)
 
 df = pd.DataFrame(phone_data)
-df.to_csv("phone1.csv", index=False)
+df.to_csv("phone2.csv", index=False)
 
 print("Data successfully extracted and CSV saved.")
